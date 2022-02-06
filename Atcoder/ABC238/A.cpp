48658b3b4bf21f8b05a694ac47a0cd9f85d37f61
Vasu Singh <85363195+vasucp1207@users.noreply.github.com>
2022-02-06 17:25:53 +0530

    Create A.cpp

---
+++ Atcoder/ABC238/A.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<iomanip>
+#include<algorithm>
+#include<vector>
+#include<utility>
+#include<set>
+#include<unordered_set>
+#include<list>
+#include<iterator>
+#include<deque>
+#include<queue>
+#include<stack>
+#include<set>
+#include<bitset>
+#include<random>
+#include<map>
+#include<unordered_map>
+#include<stdio.h>
+#include<complex>
+#include<math.h>
+#include<cstring>
+#include<chrono>
+#include<string>
+
+// #include "ext/pb_ds/assoc_container.hpp"
+// #include "ext/pb_ds/tree_policy.hpp"
+// using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update> ;
+
+#define lb lower_bound
+#define ub upper_bound
+#define ll long long
+#define pb push_back
+#define ppb pop_back
+#define fi first
+#define se second
+#define all(x) (x).begin(), (x).end()
+
+// const int mod = 1e9 + 7;
+
+using namespace std;
+
+int main(){
+
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	
+	ll n;
+	cin >> n;
+	if(n >= 5){
+		cout << "Yes" << endl;
+	}
+	else{
+		if(pow(2, n) > n * n)cout << "Yes" << endl;
+		else cout << "No" << endl;
+	}
+    
+		
+		
+
+
+	return 0;
+}
